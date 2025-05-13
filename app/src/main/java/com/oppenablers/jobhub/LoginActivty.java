1eed8b4335d32c11ea91c8f29b3c8df9ac5b830e
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2025-05-14 06:37:42 +0800

    add: Login Activity Login Activity display when Login button is pressed

--- app/src/main/java/com/oppenablers/jobhub/LoginActivty.java
+++ app/src/main/java/com/oppenablers/jobhub/LoginActivty.java
@@ -1,9 +1,6 @@
 package com.oppenablers.jobhub;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,28 +8,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class LoginActivty extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        Button loginButton = findViewById(R.id.loginButton);
-
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent loginIntent = new Intent(MainActivity.this, LoginActivty.class);
-                startActivity(loginIntent);
-            }
         });
     }
 }
