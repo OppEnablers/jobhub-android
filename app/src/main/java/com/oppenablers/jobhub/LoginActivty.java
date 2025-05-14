4dfbec3327dc67cd71449b87fd8ac270a33a1e6c
Maria Alijandra <mariaalijandra.bello@gmail.com>
2025-05-14 20:31:35 +0800

    Added Login for Job Seeker and Employer

--- app/src/main/java/com/oppenablers/jobhub/LoginActivty.java
+++ app/src/main/java/com/oppenablers/jobhub/LoginActivty.java
@@ -13,7 +13,7 @@ public class LoginActivty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login_jobseeker);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
