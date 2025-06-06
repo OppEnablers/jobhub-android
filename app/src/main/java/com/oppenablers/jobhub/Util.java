99ec6a96341fc3a125ff1d4fabbac7ff7ae876a5
Jacob Tarun <jacobtarun782@outlook.com>
2025-06-06 23:20:12 +0800

    Some helper utilities for backend

---
+++ app/src/main/java/com/oppenablers/jobhub/Util.java
@@ -0,0 +1,24 @@
+package com.oppenablers.jobhub;
+
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class Util {
+
+    public static String getTextFromTextInputLayout(TextInputLayout textInputLayout) {
+        EditText editText = textInputLayout.getEditText();
+        if (editText != null) {
+            return editText.getText().toString().trim();
+        } else {
+            return "";
+        }
+    }
+
+    public static void setTextFromTextInputLayout(TextInputLayout textInputLayout, String text) {
+        EditText editText = textInputLayout.getEditText();
+        if (editText != null) {
+            editText.setText(text.trim());
+        }
+    }
+}
