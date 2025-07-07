67caad65e51deb86767275bddc3a950c6108d0d2
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2025-07-07 10:54:22 +0800

    add: Direct Message Activity enhancment before methods

--- app/src/main/java/com/oppenablers/jobhub/activity/JsDirectMessageActivity.java
+++ app/src/main/java/com/oppenablers/jobhub/activity/JsDirectMessageActivity.java
@@ -1,6 +1,10 @@
 package com.oppenablers.jobhub.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.oppenablers.jobhub.R;
 import com.oppenablers.jobhub.databinding.ActivityMainBinding;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class JsDirectMessageActivity extends AppCompatActivity {
     @Override
@@ -21,6 +28,30 @@ public class JsDirectMessageActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Intent intent = getIntent();
+        TextView receiverTextView = findViewById(R.id.application_title);
+        EditText messageInput = findViewById(R.id.message_input);
+        ImageButton sendBtn = findViewById(R.id.send_button);
+        String userId = intent.getStringExtra("userId");
+
+        receiverTextView.setText(intent.getStringExtra("userName"));
+
+        sendBtn.setOnClickListener(v -> {
+            String messageText = messageInput.getText().toString().trim();
+
+            if (!messageText.isEmpty()) {
+                String dateKey = "date_" + System.currentTimeMillis(); // or use actual date
+
+                // Message object
+                Map<String, Object> messageData = new HashMap<>();
+                messageData.put("content", messageText);
+
+                // Send to Firebase
+
+                messageInput.setText("");
+            }
         });
     }
 }
