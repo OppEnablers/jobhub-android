3a7d97b8b539963c8244a81e80868e33475c451c
Jacob Tarun <jacobtarun782@outlook.com>
2025-07-29 00:57:59 +0800

    Touch up vacancy editor

--- app/src/main/java/com/oppenablers/jobhub/activity/EmpAddVacancyActivity.java
+++ app/src/main/java/com/oppenablers/jobhub/activity/EmpAddVacancyActivity.java
@@ -1,6 +1,9 @@
 package com.oppenablers.jobhub.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,8 @@ import com.oppenablers.jobhub.R;
 import com.oppenablers.jobhub.api.JobHubClient;
 import com.oppenablers.jobhub.databinding.ActivityEmpAddVacancyBinding;
 import com.oppenablers.jobhub.model.Vacancy;
+
+import java.util.Arrays;
 
 public class EmpAddVacancyActivity extends AppCompatActivity {
 
@@ -33,12 +38,22 @@ public class EmpAddVacancyActivity extends AppCompatActivity {
             finish();
         });
 
+        binding.jobDetailsScrollView.setOnScrollChangeListener((v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
+            binding.swipeLayout.setLockDrag(scrollY > 0);
+        });
+
         binding.confirmButton.setOnClickListener(v -> {
+
+            String jobPosition = binding.jobPosition.getText().toString();
+            String jobLocation = binding.jobLocation.getText().toString();
+            int workExp = getWorkExperienceIndex(binding.jobWorkExperience.getText().toString());
+
             Vacancy vacancy = new Vacancy(
                     "",
-                    binding.jobPosition.getText().toString(),
-                    binding.jobLocation.getText().toString()
+                    jobPosition,
+                    jobLocation
             );
+
             JobHubClient.addVacancy(vacancy, new JobHubClient.JobHubCallbackVoid() {
                 @Override
                 public void onFailure() {
@@ -51,5 +66,14 @@ public class EmpAddVacancyActivity extends AppCompatActivity {
                 }
             });
         });
+    }
+
+    private int getWorkExperienceIndex(String workExperience) {
+        String[] workExperienceArray = getResources().getStringArray(R.array.items_work_experience);
+        for (int i = 0; i < workExperienceArray.length; i++) {
+            if (workExperienceArray[i].contentEquals(workExperience))
+                return i;
+        }
+        return -1;
     }
 }
